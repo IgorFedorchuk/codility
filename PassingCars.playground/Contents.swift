a3e4af02ef4fcdefd9ba90aac7bdccfec4aa6c9b
Igor Fedorchuk <fedorchukjob@gmail.com>
2019-08-25 13:54:25 +0300

    add PassingCars

---
+++ PassingCars.playground/Contents.swift
@@ -0,0 +1,65 @@
+import UIKit
+/*
+ A non-empty array A consisting of N integers is given. The consecutive elements of array A represent consecutive cars on a road.
+
+ Array A contains only 0s and/or 1s:
+
+ 0 represents a car traveling east,
+ 1 represents a car traveling west.
+ The goal is to count passing cars. We say that a pair of cars (P, Q), where 0 ≤ P < Q < N, is passing when P is traveling to the east and Q is traveling to the west.
+
+ For example, consider array A such that:
+
+ A[0] = 0
+ A[1] = 1
+ A[2] = 0
+ A[3] = 1
+ A[4] = 1
+ We have five pairs of passing cars: (0, 1), (0, 3), (0, 4), (2, 3), (2, 4).
+
+ Write a function:
+
+ public func solution(_ A : inout [Int]) -> Int
+
+ that, given a non-empty array A of N integers, returns the number of pairs of passing cars.
+
+ The function should return −1 if the number of pairs of passing cars exceeds 1,000,000,000.
+
+ For example, given:
+
+ A[0] = 0
+ A[1] = 1
+ A[2] = 0
+ A[3] = 1
+ A[4] = 1
+ the function should return 5, as explained above.
+
+ Write an efficient algorithm for the following assumptions:
+
+ N is an integer within the range [1..100,000];
+ each element of array A is an integer that can have one of the following values: 0, 1.
+ */
+
+public func solution(_ A : inout [Int]) -> Int {
+    var zeroCount = 0
+    var result = 0
+
+    for(_, item) in A.enumerated(){
+        if item == 0{
+            zeroCount += 1
+        } else{
+            result +=  zeroCount
+        }
+
+        if result > 1000000000{
+            result = -1
+            break
+        }
+    }
+
+    return result
+}
+
+var A = [0,1,0,1,1]
+print(solution(&A))
+
